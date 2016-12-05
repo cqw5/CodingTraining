b909c3ef8a86e6d2304c847839c3c2da7de543a2
cqw5 <1040688029@qq.com>
2016-12-05 21:31:04 +0800

    update

--- CodingInterviews/Solution/24/verifySquenceOfBST.cpp
+++ CodingInterviews/Solution/24/verifySquenceOfBST.cpp
@@ -1,7 +1,7 @@
 /*! Author: qwchen
  *! Date  : 2016-11-19
  *  24. 二叉搜索树的后序遍历序列
- *  思路：在二叉搜索树的后序遍历中，最后一个数是树的根节点。数组中前面的数字可以分为两部分：
+ *  思路1：在二叉搜索树的后序遍历中，最后一个数是树的根节点。数组中前面的数字可以分为两部分：
  *            第一部分是根节点的左子树，他们都比根节点的值要小。
  *            第二部分是根节点的右子树，他们都比根节点的值要大。
  *        我们可以从数组中找到第一个大于等于根节点的值：
@@ -9,6 +9,10 @@
  *            如果该值是数组的最后一个元素（即根节点），说明根节点没有右子树；递归判断左子树序列。
  *            如果不是以上两种情况，说明根节点存在左子树和右子树，递归判断左子树序列和右子树序列。
  *            递归的终止条件是：树中只有一个结点，1个结点的话，肯定能作为后序遍历序列，直接返回true。
+ * 思路2：比思路1简洁（推荐）
+ *         后序数组的最后一个元素即为根节点，寻找数组中第一个大于等于根节点的值，则其前面的部分为左子树(可能为空)，后面的部分为右子树(可能为空)
+ *         递归的判断左右子树是否都为后序数组
+ *         
  */
 
 #include <iostream>
@@ -17,7 +21,10 @@
 
 using namespace std;
 
-class Solution {
+/*
+ * 思路1
+ */ 
+class Solution1 {
 public:
     bool VerifySquenceOfBST(vector<int> sequence) {
         if (sequence.size() == 0) { // 该题认为空序列不是空树的后序遍历
@@ -83,10 +90,44 @@ private:
     }
 };
 
+
+class Solution2 {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        if (sequence.size() <= 0) {
+            return false;
+        }
+        return verify(sequence, 0, sequence.size());
+    }
+
+private:
+    bool verify(vector<int>& sequence, int low, int high) {
+        if (low >= high) {  // 只有一个节点或者空，肯定是后序序列
+            return true;
+        }
+        int root = sequence[high];  // 根节点
+        // [low: second-1]为左子树，[second: high]为右子树
+        int second = low;
+        for (; second < high; second++) {
+            if (sequence[second] > root) {
+                break;
+            }
+        }
+        // 如果右子树中有节点小于根节点，则这不可能是一棵二叉搜索树
+        for (int i = second; i < high; i++) {
+            if (sequence[i] < root) {
+                return false;
+            }
+        }
+        // 递归判断左子数组和右子数组是否为二叉搜索树
+        return verify(sequence, low, second-1) && verify(sequence, second, high-1);
+    }
+};
+
 void testSolution(){
     vector<int> seq1 = {5,7,6,9,11,10,8}; // true
     vector<int> seq2 = {7,4,6,5};         // false
-    Solution sol;
+    Solution2 sol;
     cout << sol.VerifySquenceOfBST(seq1) << endl;
     cout << sol.VerifySquenceOfBST(seq2) << endl;
 }
