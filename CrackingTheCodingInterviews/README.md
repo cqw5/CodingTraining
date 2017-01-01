16a8c2819ce99b46199960655784c27dd3bf1b54
cqw5 <1040688029@qq.com>
2017-01-01 13:35:43 +0800

    update

--- CrackingTheCodingInterviews/README.md
+++ CrackingTheCodingInterviews/README.md
@@ -6,5 +6,6 @@
 |1.2|原串翻转|[C++](./Solution/1.2/reverseString.cpp)|数组与字符串|
 |1.3|确定两串乱序同构|[C++](./Solution/1.3/checkSam.cpp)|数组与字符串|
 |1.4|空格替换|[C++](./Solution/1.4/replaceSpace.cpp)|数组与字符串|
+|1.5|基本字符串压缩|[C++](./Solution/1.5/zipString.cpp)|数组与字符串|
 
 
---
+++ CrackingTheCodingInterviews/Solution/1.5/zipString.cpp
@@ -0,0 +1,41 @@
+/*! Author: qwchen
+ *! Date  : 2017-01-01
+ *! 字符串与数组: 1.3
+ *! 题目描述:
+ *    利用字符重复出现的次数，编写一个方法，实现基本的字符串压缩功能。
+ *    比如，字符串“aabcccccaaa”经压缩会变成“a2b1c5a3”。若压缩后的字符串没有变短，则返回原先的字符串。
+ *    给定一个string str为待压缩的串(长度小于等于10000)，保证串内字符均由大小写英文字母组成，返回一个string，为所求的压缩后或未变化的串。
+ *！测试样例
+ *    "aabcccccaaa"
+ *    返回："a2b1c5a3"
+ *    "welcometonowcoderrrrr"
+ *    返回："welcometonowcoderrrrr"
+ */
+
+/*
+ * 思路：
+ *   从头到尾遍历字符串str，用一个新的字符串newStr保存str中的字符和对应的频数。
+ *   需要注意的是，count应该转化为string类型；若压缩后的字符串没有变短，返回原先的字符串。
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ */ 
+class Zipper {
+public:
+    string zipString(string str) {
+        if (str.size() == 0) return str;
+        string newStr;
+        int i = 0, j = 1, count = 1;
+        for (; j < str.size(); j++) {
+            if (str[i] == str[j]) count++;
+            else {
+                newStr += str[i] + to_string(count);
+                count = 1;
+                i = j;
+            }
+        }
+        newStr += str[i] + to_string(count);
+        if (str.size() < newStr.size()) return str;
+        else                            return newStr;
+    }
+};
+
