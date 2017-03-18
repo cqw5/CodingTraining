731574929c9d200cfd9b1eedba591f397d2ff8a4
cqw5 <1040688029@qq.com>
2017-03-18 21:48:40 +0800

    update

--- CodingInterviews/Solution/39/treeDepth.cpp
+++ CodingInterviews/Solution/39/treeDepth.cpp
@@ -14,6 +14,9 @@ struct TreeNode {
 	}
 };*/
 
+/* 
+ * 递归版本
+ */
 class Solution {
 public:
     int TreeDepth(TreeNode* pRoot) {
@@ -25,3 +28,48 @@ public:
         return max(left, right) + 1;
     }
 };
+
+/*
+ * 非递归版本
+ * 思路：
+ *   注意到求是的高度是一个后序遍历，因此，借鉴树的后序遍历非递归版本的写法，就可以求出树的高度。
+ *   具体是，再后序遍历中，栈的大小代表树的高度。
+ */
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot) {
+        int high = 0;
+        stack<TreeNode*> s;
+        TreeNode *p = pRoot;
+        TreeNode *pre = nullptr;
+        while (p != nullptr || !s.empty()) {
+            while (p != nullptr) {
+                s.push(p);
+                if (s.size() > high) high = s.size();
+                p = p->left;
+            }
+            if (!s.empty()) {
+                p = s.top();
+                if (p->right == nullptr || p->right == pre) {
+                    pre = p;
+                    s.pop();
+                    p = nullptr;
+                }
+                else {
+                    p = p->right;
+                }
+            }
+        }
+        return high;
+    }
+};
+
