ee1c666c8cf337951eaefd6ee9ea13fb502e5090
cqw5 <1040688029@qq.com>
2017-03-18 15:44:02 +0800

    update

--- CodingInterviews/Solution/17/mergeList.cpp
+++ CodingInterviews/Solution/17/mergeList.cpp
@@ -17,6 +17,30 @@ struct ListNode {
     ListNode(int x, ListNode* theNext):val(x), next(theNext){}
 };
 
+/*
+ * 递归解法
+ */
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2) {
+        if (pHead1 == nullptr) return pHead2;
+        if (pHead2 == nullptr) return pHead1;
+        ListNode* p3;
+        if (pHead1->val <= pHead2->val) {
+            p3 = pHead1;
+            p3->next = Merge(pHead1->next, pHead2);
+        }
+        else {
+            p3 = pHead2;
+            p3->next = Merge(pHead1, pHead2->next);
+        }
+        return p3;
+    }
+};
+
+/*
+ * 迭代解法
+ */
 class Solution {
 public:
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2) {
