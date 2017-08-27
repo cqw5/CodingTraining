4c773c2e2222a26bd7b319d416c326b6e371c613
cqw5 <chenqiwei5@qq.com>
2017-08-27 16:46:31 +0800

    update

--- CodingInterviews/Solution/32/numberOf1Between1AndN.cpp
+++ CodingInterviews/Solution/32/numberOf1Between1AndN.cpp
@@ -30,6 +30,55 @@ public:
 };
 
 
+class Solution {
+public:
+    int NumberOf1Between1AndN(int n) {
+        int count = 0;
+        int len = size(n);
+        for(int i = 0; i < len; i++) {
+            count += numberOf1(n, i);
+        }
+        return count;
+    }
+    
+    int size(int n){
+        int count = 0;
+        while(n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+    
+    int numberOf1(int n, int d) {
+        int powOf10 = pow(10, d);
+        int nextPowOf10 = powOf10 * 10;
+        int roundDown = n - n % nextPowOf10;
+        int roundUp = roundDown + nextPowOf10;
+        int digit = (n / powOf10) % 10;
+        if(digit > 1) {
+            return roundUp / 10;
+        }
+        else if(digit < 1) {
+            return roundDown / 10;
+        }
+        else {
+            int right = n % powOf10;
+            return roundDown/10 + right + 1;
+        }
+        
+    }
+    
+    int pow(int base, int exp) {
+        int res = 1;
+        for(int i = 0; i < exp; i++) {
+            res *= base;
+        }
+        return res;
+    }
+};
+
+
 void testSolution() {
 	Solution sol;
 	cout << sol.numberOf1Between1AndN(12) << endl;
--- CodingInterviews/Solution/35/firstNotRepeatingChar.cpp
+++ CodingInterviews/Solution/35/firstNotRepeatingChar.cpp
@@ -16,10 +16,6 @@ using namespace std;
  * 不存在这种元素时，返回-1
  * 时间复杂度：O(n)
  * 空间复杂度：O(1)，hash只需要256个元素。
- * 
- * 改进：既然是字符，就可以用一个256位的数组来表示，数组的下标就是该字符的值（会自动转化为数字），
- *       初始化每个元素的值为-1；第一次出现时，将数值改为出现的位置；第二次出现是将数组改为-2（或者其他非-1的负数，表示重复出现）
- *       最后只需要找最小正整数的值对应的下标即可。
  */
 class Solution {
 public:
@@ -47,6 +43,28 @@ public:
             return index;
         }
         else return -1;
+    }
+};
+
+/*
+ * 改进：既然是字符，就可以用一个256位的数组来表示，数组的下标就是该字符的值（会自动转化为数字），
+ *       初始化每个元素的值为-1；第一次出现时，将数值改为出现的位置；第二次出现是将数组改为-2（或者其他非-1的负数，表示重复出现）
+ *       最后只需要找最小正整数的值对应的下标即可。
+ */
+class Solution {
+public:
+    int FirstNotRepeatingChar(string str) {
+        if(str.size() == 0) return -1;
+        vector<char> dict(256, -1);
+        for(int i = 0; i < str.size(); i++) {
+            if(dict[str[i]] == -1) dict[str[i]] = i;
+            else if(dict[str[i]] >= 0) dict[str[i]] = -2;
+        }
+        int minIndex = INT_MAX;
+        for(int i = 0; i < 256; i++) {
+            if(dict[i] >= 0 && dict[i] < minIndex) minIndex = dict[i];
+        }
+        return minIndex;
     }
 };
 
